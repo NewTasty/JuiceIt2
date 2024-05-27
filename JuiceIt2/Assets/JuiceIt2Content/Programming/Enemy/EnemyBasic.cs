9e4a240bf8f32e2513636e58807707270365a394
Corentin Renard <renardcorentin22@gmail.com>
2024-05-27 12:53:18 +0200

    Folder & Structure Unity + Prefab Override + Game Instance

--- JuiceIt2/Assets/JuiceIt2Content/Programming/Enemy/EnemyBasic.cs
+++ JuiceIt2/Assets/JuiceIt2Content/Programming/Enemy/EnemyBasic.cs
@@ -5,7 +5,7 @@ namespace JuiceIt2Content.Programming.Enemy
 {
     public class EnemyBasic : MonoBehaviour
     {
-        [SerializeField] private float speed = 300;
+        [SerializeField] private float speed = 10;
 
         private Transform _playerRef;
     
@@ -16,10 +16,16 @@ namespace JuiceIt2Content.Programming.Enemy
 
         private void Update()
         {
+            Move();
+        }
+
+        private void Move()
+        {
             Vector3 lPosition = new Vector3(transform.position.x, 1f, transform.position.z); 
             Vector3 lTargetPosition = new Vector3(_playerRef.position.x, 1f, _playerRef.position.z); 
             
             transform.position = Vector3.MoveTowards(lPosition, lTargetPosition, speed/1000); 
         }
+        
     }
 }
