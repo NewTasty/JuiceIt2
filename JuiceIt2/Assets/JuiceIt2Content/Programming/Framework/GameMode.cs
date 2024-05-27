5764c22d30bd512bd4c601798f638ee9e724d8d0
Corentin Renard <renardcorentin22@gmail.com>
2024-05-28 01:10:40 +0200

    Start spawner

--- JuiceIt2/Assets/JuiceIt2Content/Programming/Framework/GameMode.cs
+++ JuiceIt2/Assets/JuiceIt2Content/Programming/Framework/GameMode.cs
@@ -1,16 +1,35 @@
+using System;
+using JuiceIt2Content.Programming.Player.Scripts;
 using UnityEngine;
+using Random = System.Random;
 
 public class GameMode : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField, Header("Enemies Spawn rate")] private GameObject enemiesObject;
+    [SerializeField] private float baseEnemiesSpawnRate = 1;
+    [SerializeField] private float minDistanceToSpawn = 10;
+    [SerializeField] private float maxDistanceToSpawn = 15;
+    
+    private float _lTimerForSpawn;
+    private Transform _playerRef; 
+    
+    private void Start()
     {
-        
+        _playerRef = FindFirstObjectByType<PlayerEngine>().transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_lTimerForSpawn <= baseEnemiesSpawnRate)
+        {
+            _lTimerForSpawn += Time.deltaTime;
+        }
+        else
+        {
+            // float lRandomPoint = 
+            //
+            // Instantiate(enemiesObject, _playerRef);
+            _lTimerForSpawn = 0;
+        }
     }
 }
