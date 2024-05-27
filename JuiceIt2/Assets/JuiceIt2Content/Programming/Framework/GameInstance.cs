4638691fffe9757e39b52f5b348b239eef99e930
Corentin Renard <renardcorentin22@gmail.com>
2024-05-27 12:55:42 +0200

    Add Main Menu Lvl & Game Instance prefab

--- JuiceIt2/Assets/JuiceIt2Content/Programming/Framework/GameInstance.cs
+++ JuiceIt2/Assets/JuiceIt2Content/Programming/Framework/GameInstance.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
-public class GameInstance : MonoBehaviour
+namespace JuiceIt2Content.Programming.Framework
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class GameInstance : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
     }
 }
